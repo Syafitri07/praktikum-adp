a465439fce61f02b7f4c90d224818a3fc8b54e61
Syafitri07 <162285231+Syafitri07@users.noreply.github.com>
2024-05-27 19:44:38 +0700

    Add files via upload

---
+++ logo_MIT.py
@@ -0,0 +1,55 @@
+import os
+from termcolor import colored, cprint
+
+os.system ("cls")
+
+# Baris pertama
+for i in range (1):
+    cprint(" "*3, "white", "on_red", end="")
+    print(" "*2, end="")
+    cprint(" "*3, "white", "on_red", end="")
+    print(" "*2, end="")
+    cprint(" "*3, "white", "on_red", end="")
+    print(" "*2, end="")
+    cprint(" "*3, "white", "on_red", end="")
+    print(" "*2, end="")
+    cprint(" "*8, "white", "on_red", end="")
+    print()
+
+# Baris kedua
+for j in range (1):
+    cprint(" "*3, "white", "on_red", end="")
+    print(" "*2, end="")
+    cprint(" "*3, "white", "on_red", end="")
+    print(" "*2, end="")
+    cprint(" "*3, "white", "on_red", end="")
+    print()
+
+# Baris ketiga dan keempat
+for k in range (2):
+    cprint(" "*3, "white", "on_red", end="")
+    print(" "*2, end="")
+    cprint(" "*3, "white", "on_red", end="")
+    print(" "*2, end="")
+    cprint(" "*3, "white", "on_red", end="")
+    print(" "*2, end="")
+    cprint(" "*3, "white", "on_dark_grey", end="")
+    print(" "*2, end="")
+    cprint(" "*3, "white", "on_red", end="")
+    print()
+
+# Baris kelima dan keenam
+for l in range (2):
+    cprint(" "*3, "white", "on_red", end="")
+    print(" "*2, end="")
+
+    # Kolom kedua kosong
+    cprint(" "*3, end="")
+    print(" "*2, end="")
+
+    cprint(" "*3, "white", "on_red", end="")
+    print(" "*2, end="")
+    cprint(" "*3, "white", "on_dark_grey", end="")
+    print(" "*2, end="")
+    cprint(" "*3, "white", "on_red", end="")
+    print()
